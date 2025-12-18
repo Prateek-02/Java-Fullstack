0ccc68b85219803e90f627c09bb99c7372f5c4a9
Prateek <Prateekrajgrd74@gmail.com>
2025-12-18 12:31:43 +0530

    calculated areas of shapes

--- Java-Training/Questions.java
+++ Java-Training/Questions.java
@@ -1,9 +1,25 @@
-//1. write a java program to claculate area of square whose side is 4.2;
+
 
 public class Questions {
     public static void main(String[] args) {
+        //1. write a java program to claculate area of square whose side is 4.2;
         double side = 4.2;
         double area = side*side;
         System.out.println("Area of square is: " + area);
+
+        //2. write a java program to claculate area of circle whose radius is 5;
+        int r = 5;
+        double area2 = 3.14*r*r;
+        System.out.println("Area of circle is: "+ area2);
+
+        //3. write a java program to claculate area of rectangle whose l = 3.5 and b = 4;
+        double l = 3.5;
+        int b = 5;
+        double area3 = l*b;
+        System.out.println("Area of rectangle is: " + area3);
+        
+
     }
 }
+
+
