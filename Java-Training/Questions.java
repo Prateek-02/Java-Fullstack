1004314593a6137d17d0a22073bc466d945af58b
Prateek <Prateekrajgrd74@gmail.com>
2025-12-18 12:23:13 +0530

    java training

---
+++ Java-Training/Questions.java
@@ -0,0 +1,9 @@
+//1. write a java program to claculate area of square whose side is 4.2;
+
+public class Questions {
+    public static void main(String[] args) {
+        double side = 4.2;
+        double area = side*side;
+        System.out.println("Area of square is: " + area);
+    }
+}
