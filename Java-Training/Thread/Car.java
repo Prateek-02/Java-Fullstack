ac67da71c8f4d5cd0d72c745ea2e1b41ecf2503d
Prateek-02 <114800060+Prateek-02@users.noreply.github.com>
2026-01-22 12:16:23 +0530

    Updated Thread

---
+++ Java-Training/Thread/Car.java
@@ -0,0 +1,17 @@
+package Thread;
+
+public class Car extends Thread {
+
+    private final TollGate gate;
+    private final String name;
+
+    Car(TollGate gate, String name) {
+        this.gate = gate;
+        this.name = name;
+    }
+
+    @Override
+    public void run() {
+        gate.passGate(name);
+    }
+}
---
+++ Java-Training/Thread/PersonA.java
@@ -0,0 +1,15 @@
+package Thread;
+
+public class PersonA extends Thread {
+
+    private final TicketBookingSystem system;
+
+    PersonA(TicketBookingSystem system) {
+        this.system = system;
+    }
+
+    @Override
+    public void run() {
+        system.bookByChoosingSeatFirst("Rahul (Seat First)");
+    }
+}
