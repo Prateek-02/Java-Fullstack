06fda5d781e3b7181d4ed0a93bf72053fe81bc72
Prateek <Prateekrajgrd74@gmail.com>
2026-03-07 14:33:26 +0530

    SpringBoot

--- All-Capg-Workspaces/Workspace-Spring-SpringBoot/SB07-SpringBoot-Search-POST-Delete/src/main/java/com/capg/springboot/Sb07SpringBootSearchPostDeleteApplication.java
+++ All-Capg-Workspaces/Workspace-Spring-SpringBoot/SB07-SpringBoot-Search-POST-Delete/src/main/java/com/capg/springboot/Sb07SpringBootSearchPostDeleteApplication.java
@@ -8,7 +8,7 @@ public class Sb07SpringBootSearchPostDeleteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sb07SpringBootSearchPostDeleteApplication.class, args);
-		System.out.println("Server started");
+		System.out.println("Server started......");
 	}
 
 }
