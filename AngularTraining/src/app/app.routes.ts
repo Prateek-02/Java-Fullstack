765d738b40ffe6aa87ce43b917871f4cfd2fbfde
Prateek <Prateekrajgrd74@gmail.com>
2026-04-25 15:40:29 +0530

    routing

--- AngularTraining/src/app/app.routes.ts
+++ AngularTraining/src/app/app.routes.ts
@@ -1,3 +1,54 @@
 import { Routes } from '@angular/router';
+import { Forms } from './forms/forms';
+import { Dashboard } from './dashboard/dashboard';
+import { DataService } from './data-service/data-service';
+import { Navbar } from './navbar/navbar';
+import { Wild } from './wild/wild';
+import { Home } from './home/home';
+import { FormTask } from './form-task/form-task';
+import { Binding } from './binding/binding';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Binding
+    },
+    {
+        path: 'dashboard',
+        component: Dashboard,
+    },
+    {
+        path: 'forms',
+        component: Forms
+    },
+    {
+        path: 'navbar',
+        component: Navbar,
+    },
+    {
+        path: 'home',
+        component: Home,
+        children:[
+            {
+                path: 'navbar',
+                component: Navbar,
+                children:[
+                    {
+                        path: 'regform',
+                        component: FormTask
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        path: 'dataservice',
+        component: DataService
+    },
+    {
+        path: '**',
+        component: Wild
+    }
+
+];
--- AngularTraining/src/app/app.ts
+++ AngularTraining/src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { RouterOutlet, RouterLinkWithHref, Router } from '@angular/router';
 import { Binding } from "./binding/binding";
 import { Parent } from "./parent/parent";
 import { Forms } from './forms/forms';
@@ -9,10 +9,26 @@ import { Dashboard } from "./dashboard/dashboard";
 
 @Component({
   selector: 'app-root',
-  imports: [Dashboard],
+  imports: [RouterOutlet, RouterLinkWithHref],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('AngularTraining');
+  // protected readonly title = signal('AngularTraining');
+
+  route = inject(Router);
+  change: boolean = true;
+
+  login(){
+    if(this.change){
+      this.route.navigate(['/home']);
+      console.log(this.route.url);
+      this.change = false;
+    }
+    else{
+      this.route.navigate(['/forms']);
+      console.log(this.route.url);
+      this.change = true;
+    }
+  }
 }
